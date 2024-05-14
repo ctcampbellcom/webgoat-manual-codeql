73926c6e534f879810eac2ecac096e840874fb1b
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2024-05-14 13:36:52 +0100

    Test codeql on new branch (#3)

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson9.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson9.java
@@ -72,17 +72,17 @@ public class SqlInjectionLesson9 extends AssignmentEndpoint {
     try (Connection connection = dataSource.getConnection()) {
       try {
         Statement statement = connection.createStatement(TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE);
-        SqlInjectionLesson8.log(connection, query);
-        ResultSet results = statement.executeQuery(query);
-        var test = results.getRow() != 0;
-        if (results.getStatement() != null) {
-          if (results.first()) {
-            output.append(SqlInjectionLesson8.generateTable(results));
-          } else {
-            // no results
-            return failed(this).feedback("sql-injection.8.no.results").build();
-          }
-        }
+        // SqlInjectionLesson8.log(connection, query);
+        // ResultSet results = statement.executeQuery(query);
+        // var test = results.getRow() != 0;
+        // if (results.getStatement() != null) {
+        //   if (results.first()) {
+        //     output.append(SqlInjectionLesson8.generateTable(results));
+        //   } else {
+        //     // no results
+        return failed(this).feedback("sql-injection.8.no.results").build();
+          // }
+        // }
       } catch (SQLException e) {
         System.err.println(e.getMessage());
         return failed(this)
@@ -90,7 +90,7 @@ public class SqlInjectionLesson9 extends AssignmentEndpoint {
             .build();
       }
 
-      return checkSalaryRanking(connection, output);
+      // return checkSalaryRanking(connection, output);
 
     } catch (Exception e) {
       System.err.println(e.getMessage());
